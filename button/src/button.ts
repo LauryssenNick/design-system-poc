fa3fe0fc1f4798e3b1ac39aa61a427eec12f2357
Nick Lauryssen <nick.lauryssen@katoennatie.com>
2022-04-25 17:54:29 +0200

    fix(button): styles

--- button/src/button.ts
+++ button/src/button.ts
@@ -1,5 +1,6 @@
 import { LitElement, html, css } from 'lit';
 import { customElement } from 'lit/decorators.js';
+import { classMap } from 'lit/directives/class-map.js';
 
 @customElement('ktn-button')
 export default class Button extends LitElement {
@@ -7,9 +8,31 @@ export default class Button extends LitElement {
     :host {
       background-color: var(--color-primary-main);
     }
+
+    .button {
+      display: inline-flex;
+      align-items: stretch;
+      justify-content: center;
+      width: 100%;
+      border-style: solid;
+      border-width: 1px;
+      text-decoration: none;
+      user-select: none;
+      white-space: nowrap;
+      vertical-align: middle;
+      padding: 0;
+      cursor: inherit;
+    }
+
+    .button--primary {
+      background-color: var(--ktn-color-primary-medium);
+    }
   `;
 
   render() {
-    return html` <button><slot></slot></button> `;
+    return html`<button class=${classMap({
+      button: true,
+      'button--primary': true
+    })}><slot></slot></button> `;
   }
 }
