10c3d4b6812b09fe316b59f05482c299633db985
LauryssenNick <89574663+undefined@users.noreply.github.com>
2022-05-24 08:35:18 +0000

    Push from feature/tests

---
+++ button/test/index.test.js
@@ -0,0 +1,9 @@
+/**
+ * BDD-Style Testing, API inspired by https://mochajs.org/ and https://github.com/thepassle/asdgf
+ * Use any assert library you want
+ */
+describe("Dummy test", function() {
+  it("should be always true", function() {
+    console.assert(true);
+  });
+});
