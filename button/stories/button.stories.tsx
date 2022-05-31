351c2dddb9d262b54a54e83b2dc8cf6dcac52d99
LauryssenNick <89574663+undefined@users.noreply.github.com>
2022-05-31 08:29:19 +0000

    Push from feature/tests

---
+++ button/stories/button.stories.tsx
@@ -0,0 +1,11 @@
+// demo-button.stories.js
+
+import { html } from 'lit-html';
+
+import '../src/button.tsx';
+
+export default {
+  title: 'Button',
+};
+
+export const Primary = () => html`<ktn-button></demo-button>`;
